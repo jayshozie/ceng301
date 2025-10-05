a605a0c4cd65572ea206e2c3335c9b2dd4981cdc
jayshozie <143549527+jayshozie@users.noreply.github.com>
2025-10-05 22:43:14 +0300

    added functions in cpp to w01.md, nrt

--- testBench.cpp
+++ testBench.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
 
+void setIntToZero (int var) {
+    std::cout << "var = " << var << std::endl;
+    var = 0;
+    std::cout << "var = " << var << std::endl;
+}
+
 int main()
 {
-    int a, b, *p;
-
-    a = b = 7;
-    p = &a;
-    // 1st print statement
-    std::cout << "p = " << p << std::endl;
-
-    *p = 3;
-    // 2nd print statement
+    int a = 5;
     std::cout << "a = " << a << std::endl;
-
-    p = &b;
-
-    *p = 2 * *p - a;
-    // 3rd print statement
-    std::cout << "b = " << b << std::endl;
+    setIntToZero(a);
+    std::cout << "a = " << a << std::endl;
 }
