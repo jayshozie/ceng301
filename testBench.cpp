9a61388c20efba526d582a91d82bee16cd25d62f
jayshozie <143549527+jayshozie@users.noreply.github.com>
2025-10-06 21:50:41 +0300

    working on w01 references, nrt

--- testBench.cpp
+++ testBench.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
 
-void swap(int *p, int *q){
-    int tmp;
-    tmp = *p;
-    *p = *q;
-    *q = tmp;
-}
-
 int main()
 {
-    int a = 3;
-    int b = 7;
-    std::cout << a << " " << b << std::endl;
-    swap(&a,&b);
-    std::cout << a << " " << b << std::endl;
-    return 0;
+    int *p = new int;
+    *p = 10;
+    int &r = *p;
+    r++;
+    std::cout << *p << std::endl;
 }
