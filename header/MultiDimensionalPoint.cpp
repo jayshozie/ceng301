935d6a6fdab41a53e454d916766514f9dbc18a10
jayshozie <143549527+jayshozie@users.noreply.github.com>
2025-11-03 21:30:36 +0300

    implemented all method functions except last, will implement later

---
+++ header/MultiDimensionalPoint.cpp
@@ -0,0 +1,162 @@
+#include "MultiDimensionalPoint.h"
+
+// Constructor to initialize a point with a given number of dimensions.
+// It allocates memory dynamically for coordinates based on the given dimensions.
+MultiDimensionalPoint::MultiDimensionalPoint(int dim)
+    : dimension(dim), coordinates(new double[dim]) {
+}
+
+// Destructor to clean up the dynamically allocated memory for coordinates.
+MultiDimensionalPoint::~MultiDimensionalPoint() {
+    delete[] this->coordinates;
+    this->coordinates = nullptr;
+}
+
+// Copy constructor - creates a new MultiDimensionalPoint by copying another point.
+// It ensures that the copied point has its own memory for coordinates.
+MultiDimensionalPoint::MultiDimensionalPoint(const MultiDimensionalPoint& source)
+    : dimension(source.dimension) {
+        this->coordinates = new double[this->dimension];
+
+        for (int i = 0; i < this->dimension; i++) {
+            this->coordinates[i] = source.coordinates[i];
+        }
+}
+
+// not included, but copy assignment operator
+MultiDimensionalPoint& MultiDimensionalPoint::operator=(const MultiDimensionalPoint& rhs) {
+    // self-assignment check
+    if (this != &rhs) {
+        double* tmp = new double[rhs.dimension];
+
+        for (int i = 0; i < rhs.dimension; i++) {
+            tmp[i] = rhs.coordinates[i];
+        }
+
+        delete[] this->coordinates;
+        this->dimension = rhs.dimension;
+        this->coordinates = tmp;
+    }
+    return *this;
+}
+
+// Getter function to retrieve the coordinate value at a given dimension (index).
+double MultiDimensionalPoint::getCoordinate(int x) const {
+    return this->coordinates[x];
+}
+
+// Setter function to modify the coordinate value at a given dimension (index).
+void MultiDimensionalPoint::setCoordinate(int x, double value) {
+    if (x < 0 || x >= this->dimension) {
+        std::stringstream ss;
+        ss << "ERR:: " << x << " is out of range. Valid range: [0, " << this->dimension - 1 << "]" << std::endl;
+        throw std::out_of_range(ss.str());
+    }
+    this->coordinates[x] = value;
+}
+
+// Function to return the number of dimensions of the point.
+int MultiDimensionalPoint::getNumberOfDimensions() const {
+    return this->dimension;
+}
+
+// Function to print the coordinates of the point.
+// Prints each coordinate with a space in between, except for the last one.
+void MultiDimensionalPoint::print() const {
+    for (int i = 0; i < this->dimension; i++) {
+        std::cout << this->coordinates[i] << ' ';
+    }
+    std::cout << std::endl;
+}
+
+// Function to calculate the Euclidean distance between this point and another point.
+// The Euclidean distance formula is: sqrt((x1-x2)^2 + (y1-y2)^2 + ...)
+double MultiDimensionalPoint::euclideanDistance(const MultiDimensionalPoint& rhs) const {
+    // their dimensions must match
+   if (this->dimension != rhs.dimension) {
+       std::stringstream ss;
+       ss << "ERR: Dimensions don't match: " << this->dimension << " != " << rhs.dimension << std::endl;
+       throw std::out_of_range(ss.str());
+   }
+   double total = 0;
+   for (int i = 0; i < this->dimension; i++) {
+       double tmp_diff = pow(rhs.coordinates[i], 2) - pow(this->coordinates[i], 2);
+       if (tmp_diff < 0) { tmp_diff *= -1; }
+       total += tmp_diff;
+   }
+   return sqrt(total);
+}
+
+// Function to calculate the Manhattan distance between this point and another point.
+// The Manhattan distance formula is: |x1 - x2| + |y1 - y2| + ...
+double MultiDimensionalPoint::manhattanDistance(const MultiDimensionalPoint& rhs) const {
+    // their dimensions must match
+   if (this->dimension != rhs.dimension) {
+       std::stringstream ss;
+       ss << "ERR: Dimensions don't match: " << this->dimension << " != " << rhs.dimension << std::endl;
+       throw std::out_of_range(ss.str());
+   }
+   double total = 0;
+   for (int i = 0; i < this->dimension; i++) {
+       double tmp_diff = rhs.coordinates[i] - this->coordinates[i];
+       if (tmp_diff < 0) { tmp_diff *= -1; }
+       total += tmp_diff;
+   }
+   return total;
+}
+
+// Function to calculate the average of the coordinates of this point.
+double MultiDimensionalPoint::averageOfDimensions() const {
+    double total = 0;
+    for (int i = 0; i < this->dimension; i++) {
+        total += this->coordinates[i];
+    }
+    double average = total / this->dimension;
+    return average;
+}
+
+// Function to calculate the sum of all coordinates of this point.
+double MultiDimensionalPoint::sumOfDimensions() const {
+    double sum = 0;
+    for (int i = 0; i < this->dimension; i++) {
+        sum += this->coordinates[i];
+    }
+    return sum;
+}
+
+// Function to find the maximum value among the coordinates of the point.
+// Returns a pointer to the coordinate with the maximum value.
+double* MultiDimensionalPoint::maxDimension() {
+    double max = 0;
+    double* max_ptr;
+    for (int i = 0; i < this->dimension; i++) {
+        if (this->coordinates[i] >= max) {
+            max = this->coordinates[i];
+            max_ptr = &this->coordinates[i];
+        }
+    }
+    return max_ptr;
+    // doesn't make sense at all
+}
+
+// Function to find the closest point to the current point from an array of points
+MultiDimensionalPoint MultiDimensionalPoint::closestPoint(MultiDimensionalPoint* pointsArray, int numberOfPoints) const {
+    double min = MAXFLOAT;
+    MultiDimensionalPoint* closest;
+    for (int i = 0; i < numberOfPoints; i++) {
+        double distance = this->euclideanDistance(pointsArray[i]);
+        if (distance < min) {
+            min = distance;
+            closest = &pointsArray[i];
+        }
+    }
+    return *closest;
+}
+
+
+// TODO: IMPLEMENT THIS
+// Function to find the k closest points to the current point.
+// Points are provided as an array, and k is the number of closest points to find.
+MultiDimensionalPoint** MultiDimensionalPoint::kClosestPoints(MultiDimensionalPoint* pointsArray, int numberOfPoints, int k) const {
+    return nullptr;
+}
--- header/testBench.cpp
+++ header/testBench.cpp
@@ -1,20 +1,90 @@
 #include "Complex.h"
+#include "MultiDimensionalPoint.h"
+#include <iostream>
 
 int main()
 {
-    Complex a(3.14, 7.24);
-    Complex b(2.71, 1.41);
+    // Complex a(3.14, 7.24);
+    // Complex b(2.71, 1.41);
 
-    a.print();
-    b.print();
-    std::cout << a.modulus() << std::endl;
-    Complex d;
-    d = a + b;
-    d.print();
-    d = a - b;
-    d.print();
-    d = a * b;
-    d.print();
-    d = a / b;
-    d.print();
+    // a.print();
+    // b.print();
+    // std::cout << a.modulus() << std::endl;
+    // Complex d;
+    MultiDimensionalPoint x(3);
+    MultiDimensionalPoint y(2);
+    MultiDimensionalPoint z(3);
+
+    x.setCoordinate(0, 12.3);
+    x.setCoordinate(1, 34.5);
+    x.setCoordinate(2, 56.7);
+    y.setCoordinate(0, 45.6);
+    y.setCoordinate(1, 78.9);
+
+    z.setCoordinate(0, 1.1);
+    z.setCoordinate(1, 2.2);
+    z.setCoordinate(2, 3.3);
+
+    for (int i = 0; i < x.getNumberOfDimensions(); i++) {
+        std::cout << "x[" << i << "]: " << x.getCoordinate(i) << std::endl;
+    }
+    for (int i = 0; i < y.getNumberOfDimensions(); i++) {
+        std::cout << "y[" << i << "]: " << y.getCoordinate(i) << std::endl;
+    }
+    for (int i = 0; i < z.getNumberOfDimensions(); i++) {
+        std::cout << "z[" << i << "]: " << z.getCoordinate(i) << std::endl;
+    }
+
+    x.print();
+    y.print();
+    z.print();
+
+    std::cout << "Average of x: " << x.averageOfDimensions() << std::endl;
+    std::cout << "Average of y: " << y.averageOfDimensions() << std::endl;
+    std::cout << "Average of z: " << z.averageOfDimensions() << std::endl;
+
+    std::cout << "Sum of x: " << x.sumOfDimensions() << std::endl;
+    std::cout << "Sum of y: " << y.sumOfDimensions() << std::endl;
+    std::cout << "Sum of z: " << z.sumOfDimensions() << std::endl;
+
+    std::cout << "Max of x at: " << x.maxDimension() << std::endl;
+    std::cout << "Max of y at: " << y.maxDimension() << std::endl;
+    std::cout << "Max of z at: " << z.maxDimension() << std::endl;
+
+    MultiDimensionalPoint w(3);
+    w.setCoordinate(0, 9.9);
+    w.setCoordinate(1, 8.8);
+    w.setCoordinate(2, 7.7);
+    MultiDimensionalPoint v(3);
+    v.setCoordinate(0, 6.6);
+    v.setCoordinate(1, 5.5);
+    v.setCoordinate(2, 4.4);
+    MultiDimensionalPoint u(3);
+    u.setCoordinate(0, 3.3);
+    u.setCoordinate(1, 2.2);
+    u.setCoordinate(2, 1.1);
+    MultiDimensionalPoint t(3);
+    t.setCoordinate(0, 0.0);
+    t.setCoordinate(1, -1.1);
+    t.setCoordinate(2, -2.2);
+
+    // no constructor fo MultiDimensionalPoint[2];
+    MultiDimensionalPoint* exampleArray = new MultiDimensionalPoint[5];
+    exampleArray[0] = z;
+    exampleArray[1] = w;
+    exampleArray[2] = v;
+    exampleArray[3] = u;
+    
+    // closest point to x
+    MultiDimensionalPoint closest = x.closestPoint(exampleArray, 4);
+    for (int i = 0; i <= exampleArray->getNumberOfDimensions(); i++) {
+        std::cout << "exampleArray[" << i << "]: ";
+        exampleArray[i].print();
+    }
+    for (int i = 0; i < closest.getNumberOfDimensions(); i++) {
+        std::cout << "closest[" << i << "]: " << closest.getCoordinate(i) << std::endl;
+    }
+
+    std::cout << "Euclidean Distance between x and z: " << x.euclideanDistance(z) << std::endl;
+    std::cout << "Manhattan Distance between x and z: " << x.manhattanDistance(z) << std::endl;
 }
