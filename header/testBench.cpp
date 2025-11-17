799dfd1bc2c93cff1351d5bcd896c9aed695bf5b
jayshozie <143549527+jayshozie@users.noreply.github.com>
2025-11-17 09:55:56 +0300

    testing

--- header/testBench.cpp
+++ header/testBench.cpp
@@ -1,65 +1,31 @@
-#include "ArrayList.h"
-#include "Complex.h"
 #include "LinkedList.h"
-#include "StackArray.h"
-#include "StackLinked.h"
-#include <iostream>
+#include "QException.h"
 
-int main()
-{
-    // StackArray<int> intSt(5);
-    StackLinked<int> intSt;
-    intSt.push(1);
-    intSt.push(2);
-    intSt.push(3);
-    intSt.push(4);
-    intSt.push(5);
+// TODO: IMPLEMENT AFTER MT
 
-    std::cout << "intSt Top: " << intSt.peek() << std::endl;
-    std::cout << "intSt Is Empty?: " << intSt.isEmpty() << std::endl;
-    // std::cout << "Is Full?: " << intSt.isFull() << std::endl;
-    // std::cout << "Size?: " << intSt.size() << std::endl;
+template<class T> class Queue {
+private:
+    LinkedList<T>* q;
+public:
+    Queue();
+    ~Queue();
+    Queue(const Queue& rhs);
+    Queue& operator=(const Queue& rhs);
 
-    // StackArray<int> intCopySt;
-    StackLinked<int> intCopySt;
-    intCopySt = intSt;
-    std::cout << "intSt Top by Peek?: " << intSt.peek() << std::endl;
-    while(!intSt.isEmpty()) {
-        try {
-            std::cout << "intSt Pop: " << intSt.pop();
-        }
-        catch(StackException e) {
-            break;
-        }
-        std::cout << std::endl;
-    }
-    // std::cout << "intSt Size?: " << intSt.size() << std::endl;
-    std::cout << "intSt Is Empty?: " << intSt.isEmpty() << std::endl;
-    // std::cout << "intCopySt Size?: " << intCopySt.size() << std::endl;
-    // std::cout << "Is Full?: " << intCopySt.isFull() << std::endl;
-    std::cout << "intCopySt (peek) Top: " << intCopySt.peek() << std::endl;
-    std::cout << "intCopySt (pop) Top: " << intCopySt.pop() << std::endl;
-    std::cout << "intCopySt Is Empty?: " << intCopySt.isEmpty() << std::endl;
-    // std::cout << "Is Full?: " << intCopySt.isFull() << std::endl;
-    StackLinked<int> intCopyConstSt = intCopySt;
-    std::cout << "intCopyConstSt Is Empty?: " << intCopyConstSt.isEmpty()
-              << std::endl;
-    std::cout << "intCopyConstSt (peek) Top: " << intCopyConstSt.peek()
-              << std::endl;
-    std::cout << "intCopyConstSt (pop) Top: " << intCopyConstSt.pop()
-              << std::endl;
-    std::cout << std::endl << "intCopyConstSt.print(): " << std::endl;
-    intCopyConstSt.print();
-    StackLinked<int> revTest = intCopySt;
-    std::cout << std::endl << "intCopySt.print(): " << std::endl;
-    intCopySt.print();
-    std::cout << std::endl << "revTest.print(): " << std::endl;
-    revTest.print();
-    revTest.reverse();
-    std::cout << std::endl
-              << "revTest.reverse();revTest.print();: " << std::endl;
-    revTest.print();
-    std::cout << "revTest (peek) Top: " << revTest.peek() << std::endl;
-    // std::cout << "intCopyConstSt Size?: " << intCopyConstSt.size() <<
-    // std::endl;
-}
+    void enqueue(const T& value);
+    T dequeue(); // seconds as pop()
+    T peek();
+    int size() const;
+    bool isEmpty() const;
+};
+
+template<class T> Queue<T>::Queue() {}
+template<class T> Queue<T>::~Queue() {}
+template<class T> Queue<T>::Queue(const Queue<T>& rhs) {}
+template<class T> Queue<T>& Queue<T>::operator=(const Queue<T>& rhs) {}
+
+template<class T> void Queue<T>::enqueue(const T& value) {}
+template<class T> T Queue<T>::dequeue() {}
+template<class T> T Queue<T>::peek() {}
+template<class T> int Queue<T>::size() const {}
+template<class T> bool Queue<T>::isEmpty() const {}
--- header/tmp.cpp
+++ header/tmp.cpp
@@ -1,10 +1,6 @@
-#include "Complex.h"
+#include <iostream>
+#include <fstream>
 
 int main()
 {
-    Complex<double> a(9.16, 21.77);
-    Complex<float> b;
-
-    Complex<float> c;
-
 }
