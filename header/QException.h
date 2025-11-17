ce6d69c0c8234d557388452a1ce82283a282ac0c
jayshozie <143549527+jayshozie@users.noreply.github.com>
2025-11-17 09:57:07 +0300

    created QException to serve queue classes in header/

---
+++ header/QException.h
@@ -0,0 +1,14 @@
+#ifndef _QEXCEPTION_H_
+#define _QEXCEPTION_H_
+#include <sstream>
+#include <stdexcept>
+
+class QException : public std::exception {
+private:
+    std::string errStr;
+public:
+    QException(const std::stringstream& string): errStr(string.str()) {}
+    const char* what() const noexcept override { return errStr.c_str(); }
+};
+
+#endif
