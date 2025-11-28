52cf8330027a3d668a2898144390b43362229c56
jayshozie <143549527+jayshozie@users.noreply.github.com>
2025-11-28 11:53:46 +0300

    created w08, added its license

--- testBench.cpp
+++ testBench.cpp
@@ -5,3 +5,31 @@ int main()
     int avg;
     std::cout << avg << std::endl;
 }
+
+<details>
+    <summary> Solution </summary>
+
+```cpp
+class SearchTree {
+public:
+    int getMax() {
+        if(this->root == nullptr) {
+            throw new NoSuchElementException();
+        }
+        return getMax(root);
+    }
+private:
+    int getMax(IntTreeNode* root) {
+        if(root->left == nullptr) {
+            return root->data;
+        }
+        else {
+            while(root->right != nullptr) {
+                root = root->right;
+            }
+            return root->data;
+        }
+    }
+}
+```
+</details>
