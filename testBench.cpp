80761212726b5494f9f45f915a02060f0102c151
jayshozie <143549527+jayshozie@users.noreply.github.com>
2026-01-10 15:18:55 +0300

    feat: Add w09.md

--- testBench.cpp
+++ testBench.cpp
@@ -1,35 +1,40 @@
 #include <iostream>
 
+bool is_prime(int n) {
+    if(n <= 1) { return false; }
+    if(n == 2 || n == 3) { return true; }
+    if(n % 2 == 0 || n % 3 == 0) { return false; }
+    for(int i = 5; i*i<=n; i+= 6) {
+        if(n % i == 0 || n % (i + 2) == 0) { return false; }
+    }
+    return true;
+}
+
+int get_next_prime(int n) {
+    switch(n) {
+        case 0:
+            return 2;
+        case 1:
+            return 2;
+        case 2:
+            return 3;
+        case 3:
+            return 5;
+    }
+    int next_prime = n + 1;
+    if(next_prime % 2 == 0) { next_prime++; }
+    for(;;) {
+        if(is_prime(next_prime)) { return next_prime; }
+        next_prime += 2;
+    }
+}
+
 int main()
 {
-    int avg;
-    std::cout << avg << std::endl;
-}
-
-<details>
-    <summary> Solution </summary>
-
-```cpp
-class SearchTree {
-public:
-    int getMax() {
-        if(this->root == nullptr) {
-            throw new NoSuchElementException();
-        }
-        return getMax(root);
+    int next_prime;
+    for(int i = 0; i <= 100000000; i++) {
+        next_prime = get_next_prime(i);
     }
-private:
-    int getMax(IntTreeNode* root) {
-        if(root->left == nullptr) {
-            return root->data;
-        }
-        else {
-            while(root->right != nullptr) {
-                root = root->right;
-            }
-            return root->data;
-        }
-    }
+
+    printf("Last Prime : %d\n", next_prime);
 }
-```
-</details>
